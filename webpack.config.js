b5bed5208f51f050f966a0b999e61e6be578022d
Fausto <fausto.toloi@prof.sc.senac.br>
2022-08-12 19:26:32 -0300

    master

--- webpack.config.js
+++ webpack.config.js
@@ -1,67 +1,73 @@
-const path = require('path');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-module.exports = {
-    mode: 'production',
-    entry: './src/js/index.js',
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+module.exports = (env) => {
+  return {
+    mode: env.production ? 'production' : 'development',
+    entry: "./src/js/index.js",
     output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'assets/js/[name].js',
-        assetModuleFilename(module) {
-            module.filename = module.filename.normalize("NFD").replace(/[\s]/g, "_").replace(/[\u0300-\u036f]/g, "");
-            return 'assets/images/[name][ext]';
-        },
+      path: path.resolve(__dirname, "dist"),
+      filename: "assets/js/[name].js",
+      assetModuleFilename(module) {
+        module.filename = module.filename
+          .normalize("NFD")
+          .replace(/[\s]/g, "_")
+          .replace(/[\u0300-\u036f]/g, "");
+        return "assets/images/[name][ext]";
+      },
     },
     module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: [
-                            ["@babel/preset-env", {
-                                "useBuiltIns": "entry",
-                                "corejs": "3.22"
-                            }]
-                        ]
-                    }
-                }
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
+            options: {
+              presets: [
+                [
+                  "@babel/preset-env",
+                  {
+                    useBuiltIns: "entry",
+                    corejs: "3.22",
+                  },
+                ],
+              ],
             },
-            {
-                test: /\.(jpe?g|png|gif|svg)$/i,
-                type: 'asset',
-                parser: {
-                    dataUrlCondition: {
-                        maxSize: 5 * 1024
-                    }
-                }
+          },
+        },
+        {
+          test: /\.(jpe?g|png|gif|svg)$/i,
+          type: "asset",
+          parser: {
+            dataUrlCondition: {
+              maxSize: 5 * 1024,
             },
-            {
-                test: /\.(s[ac]ss|css)$/i,
-                use: [
-
-                    MiniCssExtractPlugin.loader,
-                    // Translates CSS into CommonJS
-                    "css-loader",
-                    // Loader to process CSS with PostCSS.
-                    "postcss-loader",
-                    // Compiles Sass to CSS
-                    "sass-loader",
-                ]
-            },
-        ]
+          },
+        },
+        {
+          test: /\.(s[ac]ss|css)$/i,
+          use: [
+            MiniCssExtractPlugin.loader,
+            // Translates CSS into CommonJS
+            "css-loader",
+            // Loader to process CSS with PostCSS.
+            "postcss-loader",
+            // Compiles Sass to CSS
+            "sass-loader",
+          ],
+        },
+      ],
     },
     plugins: [
-        new MiniCssExtractPlugin({
-            filename: 'assets/css/[name].css',
-        }),
-        new HtmlWebpackPlugin({
-            template: './src/index.html',
-            filename: 'index.html',
-            inject: 'body',
-        }),
+      new MiniCssExtractPlugin({
+        filename: "assets/css/[name].css",
+      }),
+      new HtmlWebpackPlugin({
+        template: "./src/index.html",
+        filename: "index.html",
+        inject: "body",
+      }),
     ],
-
+  };
 };
